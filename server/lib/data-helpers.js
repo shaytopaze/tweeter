172dd7a5baa46b0825cca9b04ea6a1e170415320
Shay Alexander <shaytopazehair@gmail.com>
2018-02-01 21:48:46 +0000

    switched database to mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,20 +8,37 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
-    },
+    // saveTweet: function(newTweet, callback) {
+    //   simulateDelay(() => {
+    //     db.tweets.push(newTweet);
+    //     callback(null, true);
+    //   });
+    // },
 
+    saveTweet: function(newTweet, callback) {
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
+    },
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
+    // getTweets: function(callback) {
+    //   simulateDelay(() => {
+    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    //   });
+    // }
+
+    getTweets: function getTweets(callback) {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets);
       });
     }
-
   };
 }
+
+
+
+
